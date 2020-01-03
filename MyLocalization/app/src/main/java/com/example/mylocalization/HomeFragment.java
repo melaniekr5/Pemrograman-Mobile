bc5207410aef8642f82fc1c8d8295226b850ec56
melaniekr5 <44056668+melaniekr5@users.noreply.github.com>
2020-01-03 11:02:47 +0700

    MyLocalization

---
+++ MyLocalization/app/src/main/java/com/example/mylocalization/HomeFragment.java
@@ -0,0 +1,47 @@
+package com.example.mylocalization;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        //return inflater.inflate(R.layout.fragment_home_fragment, container, false);
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_home,null);
+
+        TextView tvGoal = root.findViewById(R.id.tv_goal);
+        TextView tvPlural = root.findViewById(R.id.tv_plural);
+        TextView tvXliff = root.findViewById(R.id.tv_xliff);
+        int goal1 =7;
+        int goal2 =2;
+        String football = String.format(getResources().getString(R.string.goal),"Irfan Bachdim","Jamall",goal1,goal2);
+        tvGoal.setText(football);
+
+        int songCount =5;
+        String pluralText = getResources().getQuantityString(R.plurals.numberOfSongsAvailable,songCount,songCount);
+        tvPlural.setText(pluralText);
+
+        tvXliff.setText(getResources().getString(R.string.app_homeurl));
+        return root;
+    }
+
+}
