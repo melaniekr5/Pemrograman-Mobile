3678ef11363508e65ee5287ffd84883d252d26f6
melaniekr5 <44056668+melaniekr5@users.noreply.github.com>
2020-01-13 00:51:10 +0700

    MyActionBar

---
+++ MyActionBar/app/src/main/java/com/example/myactionbar/Menu2Activity.java
@@ -0,0 +1,13 @@
+package com.example.myactionbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Menu2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu2);
+    }
+}
---
+++ MyActionBar/app/src/main/java/com/example/myactionbar/MenuActivity1.java
@@ -0,0 +1,13 @@
+package com.example.myactionbar;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenuActivity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu1);
+    }
+}
